25f35cc885af5d2a18240abfc48a65f3640ecffc
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-18 02:20:05 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - use of function putchar
+ * Return - lowercase alphabet
+ */
+
+int main(void)
+{
+	char alphabet = ${a..z};
+
+	putchar(alphabet);
+
+	return (0);
+}
