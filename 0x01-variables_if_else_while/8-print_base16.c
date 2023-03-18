4c442be00b1f0b03b4b449e6e4f2609f00ee87e2
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-18 10:37:26 +0000

    Print numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - use of function putchar
- * Return - lowercase alphabet
+ * main - Entry point
+ * Description: prints all numbers of base 16
+ * Return: always zero
  */
 
 int main(void)
 {
-	char alphabet = ${a..z};
-	
-	putchar(alphabet);
+	int numbers;
+	int letters;
 
+	for (numbers = 48; numbers <= 57; numbers++) /* 48, 5 for 0 and 9 */
+	{
+		putchar(numbers);
+	}
+	for (letters = 97; letters <= 102; letters++) /*102 for letter f */
+	{
+		putchar(letters);
+	}
+	putchar('\n');
 	return (0);
 }
 
