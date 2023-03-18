fe0ff2a3d598d84fae7f0f0fa3d0c06239c2ba7a
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-18 02:45:14 +0000

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - use of function putchar
+ * Return - lowercase alphabet
+ */
+
+int main(void)
+{
+	char alphabet = {0..9};
+	
+	putchar(alphabet);
+
+	return (0);
+}
