6b3a25b91acfe619b693954407ff56559f9b8cdc
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-18 08:33:50 +0000

    Print alphabet in lowercase except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,25 @@
+#include <stdio.h>
+
 /**
- *  * main - use of function putchar
- *   * Return - lowercase alphabet
- *    */
+ * main - Entry point
+ * Description: prints alphabets in lowercase except q and e
+ * Return: lowercase alphabet
+ */
 
 int main(void)
 {
-	char alphabet = ${a..z};
-	
-	putchar(alphabet);
+	int asciiValueA = 97; /* lowercase */
 
+	while (asciiValueA <= 122) /* 122 ascii value for lowercase z */
+	{
+		if (asciiValueA == 101 || asciiValueA == 113)
+		{
+			asciiValueA++;
+			continue;
+		}
+		putchar(asciiValueA);
+		asciiValueA++;
+	}
+	putchar('\n');
 	return (0);
 }
