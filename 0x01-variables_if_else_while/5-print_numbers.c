e3bcbd78bbd141605a2ccc55fb6ae44caa7ad9a0
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-18 02:38:29 +0000

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - use of function putchar
+ *   * Return - lowercase alphabet
+ */
+
+int main(void)
+{
+	int digit = {0..9};
+
+	printf("%d\n", digit);
+
+	return (0);
+}
