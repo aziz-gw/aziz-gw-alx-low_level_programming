ca4124c7277203fb60a2e3b712c9a466083064e2
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-22 15:14:11 +0000

    Print every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int h1, m1, h2, m2; /* hour1, minute1, hour2, minute2 */
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (m1 = 0; m1 <= 9; m1++)
+		{
+			for (h2 = 0; h2 <= 5; c++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					if (a >= 2 && b >= 4)
+						break;
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n')
+				}
+			}
+		}
+	}
+}
