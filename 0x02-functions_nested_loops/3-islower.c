597bdd4395bdca21dcbb180e7e5f65d78dd785cd
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-22 10:45:29 +0000

    Print 1 if char is lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - prints lowcase character (c)
+ * @c: ASCII character
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
