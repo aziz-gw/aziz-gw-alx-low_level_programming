22a191511b91d00a25d69078d293ce08e729ef96
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-27 14:26:03 +0000

    Update a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of an integer, n
+ * to 98
+ * @n: integer
+ *
+ * Return: updated value
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
