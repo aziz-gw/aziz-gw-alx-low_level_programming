d6dc683b51a90f53163b18414a997ed850b4e535
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-03-28 06:02:45 +0000

    Print elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,20 +4,21 @@
 /**
  * print_array - print n elements of an array of integers
  * @n: integer
+ * @a: an array
  * Return: void
  */
 
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-		if (i != (n - 1))
-		{
-			printf(", ");
-		}
+	printf("%d", a[i]);
+	if (i != (n - 1))
+	{
+	printf(", ");
+	}
 	}
 	printf("\n");
 
