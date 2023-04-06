b91a25b80bca8f4ec885ed9e4e07224657565d9c
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-06 12:25:46 +0000

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(s[i]);
+}
