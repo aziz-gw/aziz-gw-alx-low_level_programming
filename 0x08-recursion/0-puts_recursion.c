5bbc10f9c633eda3c2c98534aa5b50eeb8a17e45
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-06 09:20:57 +0000

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, s
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	s++;
+	_puts_recursion(s);
+}
