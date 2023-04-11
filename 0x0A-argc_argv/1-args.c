a4d3cd483832d4db151ea696ddad6f5c9742b024
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-11 03:47:19 +0000

    Print a number of args passed to a program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
