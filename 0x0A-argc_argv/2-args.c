f70a0d409226483f37f9a18cb1cc5b7a375319bf
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-11 06:08:50 +0000

    Print number of args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,12 @@
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
+	int i = 0;
 
-	if (argc != 0)
+	while (argv[i] != 0)
 	{
-		for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+		printf("\n%s\n\n", argv[i]);
+		i++;
 	}
 	return (0);
 }
