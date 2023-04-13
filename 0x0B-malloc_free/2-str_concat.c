cc3aebd08a620d02d4305e2056a9c684526da6a5
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-13 02:15:05 +0000

    Concat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,14 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr = malloc((sizeof(char) * (strlen(s1) + strlen(s2) + 1)));
 
-	strcpy(ptr, s1);
-	strcat(ptr, s2);
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	strcpy(ptr, s1);
+	strcat(ptr, s2);
+
 	if (ptr == NULL)
 		return (NULL);
 	return (ptr);
