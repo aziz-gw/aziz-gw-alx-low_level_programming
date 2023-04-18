587da678c9501af2ba42dc3196b960b6e10a570b
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-18 10:03:36 +0000

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,14 +17,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL)
+		return (NULL);
+
+	if (owner == NULL)
 		return (NULL);
 
 	new_dog->name = strdup(name);
 	new_dog->age = age;
 
 	new_dog->owner = strdup(owner);
-	new_dog->owner = owner;
 
 	return (new_dog);
 }
