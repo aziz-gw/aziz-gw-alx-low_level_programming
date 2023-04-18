2149b23f89744eb2ff77726bba2302ee1b912721
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-18 01:57:24 +0000

    Define struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - strucct with details of a dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
