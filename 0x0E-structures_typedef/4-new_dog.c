30a9d45b3426fb4b89ad10a8756993d50b488b2f
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-18 09:35:21 +0000

    Create a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner:owner
+ * Return: name and owner and NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (name == NULL)
+		return (NULL);
+
+	if (owner == NULL)
+		return (NULL);
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+
+	new_dog->owner = strdup(owner);
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
