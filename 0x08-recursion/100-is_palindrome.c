232f8e161ea0cb788f5cfa79aa66cb8ec7cdc92e
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-23 06:53:17 +0000

    Check palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,19 +4,38 @@
 /**
  * is_palindrome - checks if string is a palindrome
  * @s: string
+ * @start: beginning of string
+ * @end: end of striing
  *
  * Return: 1 is a string is a palindrome and 0 if not
  */
 
+int check_palindrome(char *s, int start, int end);
+
 int is_palindrome(char *s)
 {
-	int i;
 	int s_len = strlen(s);
 
-	for (i = 0; i < s_len / 2; i++)
-	{
-		if (s[i] != s[s_len - i - 1])
-			return (0);
-	}
-	return (1);
+	return (check_palindrome(s, 0, s_len - 1));
+}
+
+/**
+ * check_palindrome - checks if string is a palindrome
+ * @s: string
+ * @start: beginning of string
+ * @end: end of string
+ *
+ * Return: 1 if first and last chars of s match
+ * and 0 if they don't match
+ */
+
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] == s[end])
+		return (check_palindrome(s, start + 1, end - 1));
+
+	return (0);
 }
