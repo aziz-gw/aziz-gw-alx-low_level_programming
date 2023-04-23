15be3ca4f84b23a0aba76ed794d3400107e38a67
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-23 02:15:54 +0000

    Return length of prefixed substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,18 +2,30 @@
 
 /**
  * _strspn - gets length of a prefix substring
+ * @s: first string
+ * @accept: second string
  *
+ * Return: number of bytes
  *
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int byte_num, i;
+	int byte_num, i, j;
 
+	byte_num = 0;
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == accept[j])
-			byte_num = accept[j];
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				byte_num++;
+				break;
+			}
+		}
+		if (!accept[j])
+			return (byte_num);
 	}
 	return (byte_num);
 }
