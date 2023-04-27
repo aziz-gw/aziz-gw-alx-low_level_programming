ad221f1330bf42faa54cc6212b81a17037f0aa32
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-27 23:02:06 +0000

    Print a string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_str - prints a string
+ *
+ * Return: 0;
+ */
+
+void print_str(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
