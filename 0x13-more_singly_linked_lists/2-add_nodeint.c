575eb3775b0d8938b8c99ee8af08c1147538e0c0
Aziz Nagwaka <ngwk.zz@outlook.com>
2023-04-29 02:39:42 +0000

    Add new node at the beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,34 +2,24 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint_end - adds a new node at the end of a list
- * @head: pointer that points to another pointer
+ * add_nodeint - adds a new node at the beginning of a list
+ * @head: head pointer points to the pointer that points
+ * to the next element in the list
  * @n: new node
  *
  * Return: new node
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
-	listint_t *currNode = *head;
 
 	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = n;
-	newNode->next = NULL;
-
-	if (*head == NULL)
-		*head = newNode;
-	else
-	{
-		while (currNode->next != NULL)
-			currNode = currNode->next;
-	}
-	currNode->next = newNode;
+	newNode->next = *head;
+	*head = newNode;
 
 	return (newNode);
 }
-
-
