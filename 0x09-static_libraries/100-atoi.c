759747707e1652fb78894b6710c97149aebdba2b
Aziz <ngwk.zz@outlook.com>
2023-06-09 19:52:17 +0000

    Static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  * _atoi - converts a string to an integer
+ *   * @s: string
+ *    *
+ *     * Return: an integer
+ *      */
+
+int _atoi(char *s)
+{
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * _isalpha - checks for alphabet character (c)
+ *   * @c: ASCII character
+ *    * Return: 1 if c is a letter, lowercase
+ *     * or uppercase
+ *      * otherwise, 0
+ *       */
+
+int _isalpha(int c)
+{
+	        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+			        {
+					                return (1);
+							        }
+		        else
+				        {
+						                return (0);
+								        }
+			        _putchar('\n');
+}
