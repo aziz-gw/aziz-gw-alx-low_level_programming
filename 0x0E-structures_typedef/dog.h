18bc754375389c89255100121c744e3358512f81
Aziz <ngwk.zz@outlook.com>
2023-06-19 10:17:56 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that contains a dog's properties
+ * and owner
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ *
+ * Description:  structure that contains a dog's properties
+ * and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
