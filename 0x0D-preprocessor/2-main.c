2a6433288c901295ee8d2e13896c5b5dd0cd9b55
Aziz <ngwk.zz@outlook.com>
2023-06-19 05:17:24 +0000

    Print the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file the program was
+ * compiled from
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
