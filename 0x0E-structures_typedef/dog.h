5e1aa51b780690570549a290c84aef0f951444a3
Aziz <ngwk.zz@outlook.com>
2023-06-19 15:50:39 +0000

    struct dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,25 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * struct dog_t - structure that contains a dog's properties
+ * and owner
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ *
+ * Description:  structure that contains a dog's properties
+ * and owner
+ */
+
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
 
