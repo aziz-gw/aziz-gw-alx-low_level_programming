313743ba64d440fa774e9cb222915d2ef119f604
Aziz <ngwk.zz@outlook.com>
2023-06-23 14:35:02 +0000

    Opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *p_main;
+
+	p_main = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+		printf("%02hhx", p_main[i]);
+
+	printf("\n");
+
+	return (0);
+}
