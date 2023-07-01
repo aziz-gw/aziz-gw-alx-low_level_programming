b8fafcd58d196944ab09bb7c0b6cd54d42e92770
Aziz <ngwk.zz@outlook.com>
2023-07-01 15:08:41 +0000

    Print a string

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,7 @@ void print_str(void);
 
 void __attribute__((constructor)) print_str(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
 
