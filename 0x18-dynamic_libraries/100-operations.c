65a6126f51514271ac189e6a6d6924a0f5eb0508
Aziz <ngwk.zz@outlook.com>
2023-08-21 19:51:13 +0000

    Dynamic lib 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+/**
+ * add - adds two nums
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two nums
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two nums
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of two nums
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two nums
+ * @a: first num
+ * @b: second num
+ *
+ * Return: result of operation
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
